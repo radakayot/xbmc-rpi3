73d41014b9048fa43ebd237cd115685e53d227be
radakayot <arda@oktay.me>
2023-04-20 14:50:48 +0000

    Change callback and input buffer count.

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMMAL.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMMAL.cpp
@@ -387,7 +387,7 @@ bool CDVDVideoCodecMMAL::Open(CDVDStreamInfo& hints, CDVDCodecOptions& options)
                                   hints.ptsinvalid ? MMAL_TRUE : MMAL_FALSE);
 
   mmal_port_parameter_set_uint32(m_input, MMAL_PARAMETER_VIDEO_MAX_NUM_CALLBACKS,
-                                 -1 - MMAL_CODEC_NUM_BUFFERS); //DPB=3+4, 7 total
+                                 -10); //DPB=3+9, 12 total
 
   if (mmal_port_format_commit(m_input) != MMAL_SUCCESS)
   {
@@ -395,7 +395,7 @@ bool CDVDVideoCodecMMAL::Open(CDVDStreamInfo& hints, CDVDCodecOptions& options)
     return false;
   }
 
-  m_input->buffer_num = MMAL_CODEC_NUM_BUFFERS * 2;
+  m_input->buffer_num = 12;
   m_input->buffer_size = m_input->buffer_size_min;
 
   if (m_input->buffer_alignment_min > 0)
