4f5c0c8e7a6ac5ed4cf0c9096a7d7ac94d8af130
radakayot <arda@oktay.me>
2023-04-21 10:59:39 +0000

    Test with reduced callbacks.

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMMAL.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecMMAL.cpp
@@ -387,7 +387,7 @@ bool CDVDVideoCodecMMAL::Open(CDVDStreamInfo& hints, CDVDCodecOptions& options)
   mmal_port_parameter_set_boolean(m_input, MMAL_PARAMETER_VIDEO_INTERPOLATE_TIMESTAMPS,
                                   hints.ptsinvalid ? MMAL_TRUE : MMAL_FALSE);
 
-  mmal_port_parameter_set_uint32(m_input, MMAL_PARAMETER_VIDEO_MAX_NUM_CALLBACKS, -10);
+  mmal_port_parameter_set_uint32(m_input, MMAL_PARAMETER_VIDEO_MAX_NUM_CALLBACKS, -MMAL_CODEC_NUM_BUFFERS);
 
   if (mmal_port_format_commit(m_input) != MMAL_SUCCESS)
   {
